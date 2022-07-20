43c97353e505bdbf9a16a93dc86375f9a60f8a87
diego-av <vazquezdiegoda@gmail.com>
2022-07-20 12:52:43 -0300

    listas

---
+++ listas.js
@@ -0,0 +1,14 @@
+let lista = [
+  "Diego",
+  34,
+  true,
+  new Date(1987, 8, 28),
+  {
+    titulo: "Harry Potter",
+    autor: "Rowling",
+    fecha: "1997",
+    url: "https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal",
+  },
+];
+
+console.log(lista);
